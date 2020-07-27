f95f3b381611bbd0a1bd368d470c299450357f3d
FEBI FEBRIANSYAH <febi1900018129@webmail.uad.ac.id>
2020-07-27 14:01:54 +0700

    project pertama

---
+++ assets/mail/contact_me.php
@@ -0,0 +1,22 @@
+<?php
+
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+  http_response_code(500);
+  exit();
+}
+
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+
+$to = "yourname@yourdomain.com"; 
+$subject = "Website Contact Form:  $name";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
+$header = "From: noreply@yourdomain.com\n"; 
+$header .= "Reply-To: $email";	
+
+if(!mail($to, $subject, $body, $header))
+  http_response_code(500);
+?>
